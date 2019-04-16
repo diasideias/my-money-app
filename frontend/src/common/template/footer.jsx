a3f01de6467a3e084c2bb125f57c60e17433bebd
Dias Ideias <49440332+diasideias@users.noreply.github.com>
2019-04-16 19:30:02 -0300

    Add files via upload

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2019
+            <a href='http://www.diasideias.com' target='_blank'> DiasIdeias</a>.
+        </strong>
+    </footer>
+)
